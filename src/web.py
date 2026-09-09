c6b3d2085140abc59a231133937a4d2ae87720b6
Tejasri <tejasrisunku@gmail.com>
2026-09-09 15:48:56 +0530

    Add FastAPI web app: GST calc endpoint + input form

--- src/web.py
+++ src/web.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from src.product import Product
+from fastapi.responses import HTMLResponse
+
 app = FastAPI()
 
 @app.get("/")
@@ -11,3 +13,17 @@ def home():
 def calc(base: int, rate: int, interstate: bool = False):
     p = Product("item", base, rate, interstate)
     return p.calculate_gst()
+
+
+
+@app.get("/form", response_class=HTMLResponse)
+def form():
+    return """
+    <h2>GST Calculator</h2>
+    <form action="/calc" method="get">
+        Base price: <input name="base" type="number"><br>
+        Rate: <input name="rate" type="number"><br>
+        Interstate: <input name="interstate" value="false"><br>
+        <button type="submit">Calculate</button>
+    </form>
+    """
